9ecb78c1247bb0b14ebf4791a575864543052e80
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-01-29 23:30:35 +0530

    colors and fonts configured

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
     <main className="container mx-auto flex h-[100vh] items-center justify-center">
       <div>
         <h1 className="text-3xl font-normal text-red-400">3- card component</h1>
-        <h2 className="font-heading text-3xl font-bold">heading2</h2>
+        <h2 className="font-heading text-3xl font-bold text-veryDarkCyan">
+          heading2
+        </h2>
       </div>
     </main>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,13 @@ module.exports = {
       fontFamily: {
         heading: ["Big Shoulders Display", "cursive"],
       },
+      colors: {
+        brightOrange: "hsl(31, 77%, 52%)",
+        darkCyan: "hsl(184, 100%, 22%)",
+        veryDarkCyan: "hsl(179, 100%, 13%)",
+        transparentWhite: "hsla(0, 0%, 100%, 0.75)",
+        veryLightGray: "hsl(0, 0%, 95%)",
+      },
     },
   },
   plugins: [],
