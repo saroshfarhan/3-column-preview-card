4182866f364bd16cb30034b35bd28afd707264d1
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-01-29 22:27:26 +0530

    initial setup.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <main className="container mx-auto">
+      <h1 className="text-3xl font-bold text-red-400">3- card component</h1>
+    </main>
+  );
+}
+
+export default App;
