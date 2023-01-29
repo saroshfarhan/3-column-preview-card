3c449b3f25de1ca1ea9b75f821b9f35aa6514582
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-01-29 22:34:01 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
   return (
-    <main className="container mx-auto">
+    <main className="container mx-auto flex h-[100vh] items-center justify-center">
       <h1 className="text-3xl font-bold text-red-400">3- card component</h1>
     </main>
   );
